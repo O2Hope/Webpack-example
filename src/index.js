b5f6201e5cb503f716f04fb4f32a3095d30ce483
Agustin Ruiz <agustinruiz@MacBook-Air-de-Agustin.local>
2019-09-07 22:28:28 -0500

    Add new component to validate styles in only one file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
 import addImage from "./add-image"
+import Header from "./components/header/header"
 
 const helloWorlfButton = new HelloWorldButton();
+const header = new Header();
 
+header.render();
 helloWorlfButton.render();
 addImage();
